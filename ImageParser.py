a52e40d611dbaaa1054ffc92b054ceb4c1804511
redaah <rafvace@gmail.com>
2018-09-03 00:04:00 +0200

    lets go

--- ImageParser.py
+++ ImageParser.py
@@ -1,5 +1,6 @@
 # Imports the Google Cloud client library
 from google.cloud import bigquery
+import google.cloud  import cloudstorage
 import os
 
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:/Users/Rafael/Desktop/Temp/bigquerytest-2e2156128650.json'
@@ -18,7 +19,17 @@ table_ref = dataset_ref.table(MyTable)
 
 table = client.get_table(table_ref)  # API Request
 
-# View table properties
-print(table.schema)
-print(table.description)
-print(table.num_rows)
+
+# Use the start index to load an arbitrary portion of the table
+rows = client.list_rows(table, start_index=10, max_results=1)
+
+# Print row data in tabular format
+format_string = '{!s:<16} ' * len(rows.schema)
+field_names = [field.name for field in rows.schema]
+#print(format_string.format(*field_names))  # prints column headers
+for row in rows:
+    print(row.gcs_url)     # prints row data
+    gcs_file = cloudstorage.open(row.gcs_url)
+    contents = gcs_file.read()
+    gcs_file.close()
+
---
+++ cloudstorage.py
@@ -0,0 +1,10 @@
+from google.cloud import storage
+client = storage.Client()
+# https://console.cloud.google.com/storage/browser/[bucket-id]/
+bucket = client.get_bucket('bucket-id-here')
+# Then do other things...
+blob = bucket.get_blob('remote/path/to/file.txt')
+print(blob.download_as_string())
+blob.upload_from_string('New contents!')
+blob2 = bucket.blob('remote/path/storage.txt')
+blob2.upload_from_filename(filename='/local/path.txt')
