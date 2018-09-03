4d6eee30c1ee3016086388d45424cdcda624d475
Rafael Vaquero <rvaquero@petroprix.com>
2018-09-03 20:00:49 +0200

    looking for an option with http url

--- ImageParser.py
+++ ImageParser.py
@@ -1,7 +1,7 @@
 # Imports the Google Cloud client library
 from google.cloud import bigquery
 import os
-import cloudstorage
+#import cloudstorage
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='/Users/rafa/Documents/Thesis/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
 
@@ -9,6 +9,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:/Users/Rafael/PycharmProjects/Im
 MyProject='bigquery-public-data'
 MyDataset='the_met'
 MyTable='images'
+MyBucket='gcs-public-data--met'
+MyFolder='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/'
 
 #References
 client = bigquery.Client(project=MyProject)
@@ -18,15 +20,11 @@ table_ref = dataset_ref.table(MyTable)
 
 table = client.get_table(table_ref)  # API Request
 
-
 # Use the start index to load an arbitrary portion of the table
 rows = client.list_rows(table, start_index=10, max_results=1)
 
-# Print row data in tabular format
-format_string = '{!s:<16} ' * len(rows.schema)
-field_names = [field.name for field in rows.schema]
-#print(format_string.format(*field_names))  # prints column headers
 for row in rows:
-    print(row.gcs_url)     # prints row data
-
+    FileName=(row.gcs_url.replace('gs://', '').replace(MyBucket+'/', ''))     # prints row data
+    print(FileName)
+    #cloudstorage.ImageDownload(FileName,MyFolder)
 
--- cloudstorage.py
+++ cloudstorage.py
@@ -5,17 +5,24 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
 MyProject='bigquery-public-data'
 MyBucket='gcs-public-data--met'
-#blob_name='10871/0.jpg'
+blob_name='10871/0.jpg'
+MyFolder='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/'
 #delimiter=''
+client = storage.Client(project=MyProject)
+bucket = client.get_bucket(MyBucket)
 
 def ImageDownload(blob_name, folder):
-    client = storage.Client(project=MyProject)
     # https://console.cloud.google.com/storage/browser/[bucket-id]/
-    bucket = client.get_bucket(MyBucket)
-
     blob=bucket.blob(blob_name)
     blob.download_to_filename(folder+blob_name.replace('/', '_'))
 
 
 if __name__=='__main__':
-    ImageDownload('10871/0.jpg','C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/')
+    #ImageDownload('437814/1.jpg','C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/')
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/Rafael/PycharmProjects/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
+    prefix='437814/object.json'
+    blobs = bucket.list_blobs(prefix=prefix)
+    print('Blobs:')
+    for blob in blobs:
+        print(blob.name)
+        blob.download_to_filename(MyFolder + blob.name.replace('/', '_'))
