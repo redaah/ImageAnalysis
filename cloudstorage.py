6d9d729cfa346a6cdf5a73201a837027ef246f21
Rafael Vaquero <rvaquero@petroprix.com>
2018-09-04 20:06:33 +0200

    added samplejson

--- cloudstorage.py
+++ cloudstorage.py
@@ -1,31 +1,37 @@
 from google.cloud import storage
 import os
 
-
+#Variables definition
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
 MyProject='bigquery-public-data'
 MyBucket='gcs-public-data--met'
 blob_name='10871/0.jpg'
 MyFolder='C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/'
-#delimiter=''
 client = storage.Client(project=MyProject)
 bucket = client.get_bucket(MyBucket)
 
-def ImageDownload(blob_name, folder):
-    # https://console.cloud.google.com/storage/browser/[bucket-id]/
-    blob=bucket.blob(blob_name)
-    blob.download_to_filename(folder+blob_name.replace('/', '_'))
+def download_all_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        download_blob(bucket.name, blob.name, MyFolder+blob.name.replace('/', '_'))
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
 
 
 if __name__=='__main__':
-    #ImageDownload('437814/1.jpg','C:/Users/Rafael/PycharmProjects/ImageAnalysis/Images/')
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/Users/Rafael/PycharmProjects/ImageAnalysis/Authentication/MyPython-b4f57d70838f.json'
-    prefix='437814/object.json'
-    blobs = bucket.list_blobs(prefix=prefix)
-    print('Blobs:')
-    for blob in blobs:
-        print(blob.name)
-        blob.download_to_filename(MyFolder + blob.name.replace('/', '_'))
+
+    download_all_blobs(bucket.name)
 
 
 
