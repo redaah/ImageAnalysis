520fee315379d9600def3e3b2a2a268abd3a64a1
Rafael Vaquero <rvaquero@petroprix.com>
2018-09-06 19:30:08 +0200

    ejercicio de keras

--- KerasExcercises.py
+++ KerasExcercises.py
@@ -1,7 +1,66 @@
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
 
 
+
+####Creating the network
+#Sequential or graph, sequential for layers
+classifier = Sequential()
+
+#Convolution images with the kernel
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+#Pooling results
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+#Transform into streams
+classifier.add(Flatten())
+#Hidden units with relu activation function
+classifier.add(Dense(units = 128, activation = 'relu'))
+#Output Sigmoid function
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+#Compile network and indicate loss function, optimization function and metrics for optimization
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+######
+
+###Preparing the data
+train_datagen = ImageDataGenerator(rescale = 1./255,
+shear_range = 0.2,
+zoom_range = 0.2,
+horizontal_flip = True)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory('training_set',
+target_size = (64, 64),
+batch_size = 32,
+class_mode = 'binary')
+test_set = test_datagen.flow_from_directory('test_set',
+target_size = (64, 64),
+batch_size = 32,
+class_mode = 'binary')
+####
+
+classifier.fit_generator(training_set,
+steps_per_epoch = 8000,
+epochs = 25,
+validation_data = test_set,
+validation_steps = 2000)
+
+
+
+###Making predictions
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+a=1
